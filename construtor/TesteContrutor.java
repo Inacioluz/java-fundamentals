207179cb29ebbcf6d6deccc6f79f942cc1d8fcb5
Inacio Silva <joseinaciolds@gmail.com>
2023-11-04 00:14:40 -0300

    Construtor

---
+++ construtor/TesteContrutor.java
@@ -0,0 +1,13 @@
+package construtor;
+
+public class TesteContrutor {
+    public static void main(String[] args) {
+        
+        Construtor construtor = new Construtor(1 , "text");
+        Construtor construtor1 = new Construtor(2);
+
+        System.out.println(construtor);
+        System.out.println(construtor1);
+
+    }
+}
