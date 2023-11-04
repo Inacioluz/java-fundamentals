5406a9fdeace48de0793e709fe462f3e4e5eb5c7
Inacio Silva <joseinaciolds@gmail.com>
2023-11-04 00:40:24 -0300

    wreapper

---
+++ wrappear/Wreapper.java
@@ -0,0 +1,23 @@
+package wrappear;
+
+public class Wreapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 2;
+        boolean tipoBoolean = false;
+
+        // Tipo primitivo (int)
+int numero = 10;
+
+// Classe de invólucro (Integer)
+Integer numeroObjeto = new Integer(10);
+
+// Autoboxing: conversão automática de tipo primitivo para objeto
+Integer numeroAutoboxing = 10;
+
+// Unboxing: conversão automática de objeto para tipo primitivo
+int numeroDesempacotado = numeroAutoboxing;
+
+
+    }
+}
